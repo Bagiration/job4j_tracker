3e32c97b98b0a19aac70830462b2a1b49d6ec0b6
Bagiration <Bagiration@mail.ru>
2021-01-08 08:55:23 +0300

    1.1.1. Конструктор по умолчанию. [#285442]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    String desc;
+
+    public Fix(String text) {
+        text = desc;
+    }
+
+    public static void main(String[] args) {
+        Fix Car = new Car("BMW");
+    }
+}
