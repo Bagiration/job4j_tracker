1329ebed9f78e9686b2e392cefab0ff409bd6b2d
Bagiration <Bagiration@mail.ru>
2021-01-16 15:40:02 +0300

    3. Удаление моделей из массива. [#285456]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,14 +2,12 @@ package ru.job4j.pojo;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-        int p = 0;
+        int p = -1;
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product == null) {
                 p = i;
                 break;
-            } else {
-                p = -1;
             }
         }
         return p;
