876d02feb7c062793a6df41946821e66cedb3eb7
Bagiration <Bagiration@mail.ru>
2021-01-23 01:57:10 +0300

    5. Tracker - хранилище [#285460]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,15 +1,44 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
-/**
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
+
+    public Item[] findAll() {
+        Item[] itemsWhisoutNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                itemsWhisoutNull[size] = items[i];
+                size++;
+            }
+        }
+        itemsWhisoutNull = Arrays.copyOf(itemsWhisoutNull, size);
+        return itemsWhisoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsWhisoutNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getName() == key) {
+                itemsWhisoutNull[size] = items[i];
+                size++;
+            }
+        }
+        itemsWhisoutNull = Arrays.copyOf(itemsWhisoutNull, size);
+        return itemsWhisoutNull;
+    }
+
 
     public Item findById(int id) {
         Item rsl = null;
@@ -22,5 +51,4 @@ public class Tracker {
         }
         return rsl;
     }
- */
 }
